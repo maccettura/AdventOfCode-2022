435e58e6a217495ef1c44269f89af5f9ae40346d
Michelangelo Accettura <MAccettura@rightpoint.com>
2022-12-06 12:04:46 -0500

    Adding part 4

--- Source/Day04/Solution.cs
+++ Source/Day04/Solution.cs
@@ -1,18 +1,54 @@
-﻿namespace AdventOfCode.Day04;
+﻿using System.Linq;
+
+namespace AdventOfCode.Day04;
 
 public class Solution : BaseSolution
 {
-    public Solution() : base(4, "")
+    public Solution() : base(4, "Camp Cleanup")
     {
     }
 
     public override string GetPart1Answer()
     {
-        return string.Empty;
+        var assignments = GetResourceString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        int overlappingTotal = 0;
+        foreach(var assignment in assignments)
+        {
+            var sub = assignment.Split(',');
+
+            var first = sub[0].Split('-').Select(int.Parse).ToArray();
+            var second = sub[1].Split('-').Select(int.Parse).ToArray();
+
+            if (first[0] >= second[0] && first[1] <= second[1])
+            {
+                overlappingTotal++;
+            }
+            else if (second[0] >= first[0] && second[1] <= first[1])
+            {
+                overlappingTotal++;
+            }
+        }
+        return overlappingTotal.ToString();
     }
 
     public override string GetPart2Answer()
     {
-        return string.Empty;
+        var assignments = GetResourceString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        int overlappingTotal = 0;
+        foreach (var assignment in assignments)
+        {
+            var sub = assignment.Split(',');
+
+            var first = sub[0].Split('-').Select(int.Parse).ToArray();
+            var second = sub[1].Split('-').Select(int.Parse).ToArray();
+
+            if (first[0] <= second[1] && second[0] <= first[1])
+            {
+                overlappingTotal++;
+            }
+        }
+        return overlappingTotal.ToString();
     }
 }
